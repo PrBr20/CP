50259de5aa37a94d211fa21ffb1d0dbf0db7de66
PrBr20 <protoybarai@gmail.com>
2024-09-28 04:07:12 +0600

    updated

--- CP_files/CP_1.cpp
+++ CP_files/CP_1.cpp
@@ -20,36 +20,6 @@ int a[N], tripls[N];
 vector<int> divi[N];
  
 void solve() {
-  int n;
-  cin>>n;
-  for(int i=0;i<n;i++) cin>>a[i];
-  sort(a,a+n);
-  
-  for(int i=0;i<n;i++) {
-    for(int j=1;j*j<=a[i];j++) {
-      if(a[i]%j==0) {
-        divi[j].pb(i);
-        if(j != a[i]/j) divi[a[i]/j].pb(i); 
-      }
-    }
-  }
-  
-  for(int i=N-1;i>=1;i--) {
-    for(int j=0;j<divi[i].size();j++) {
-      int l=j, r=n-1-divi[i][j];
-      tripls[i]+=l*r;
-    }
-    for(int j=2*i;j<N;j+=i) tripls[i]-=tripls[j];
-  }
-  
-  int ans=0;
-  for(int i=1;i<N;i++) ans+=tripls[i]*i;
-  cout<<ans<<endl;
-
-  for(int i=0;i<N;i++) {
-    tripls[i]=0;
-    divi[i].clear();
-  }
   
 }
   
